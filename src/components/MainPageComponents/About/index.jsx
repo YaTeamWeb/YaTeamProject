f03ac89128fe011faa30c491c60ff9ce691213d3
W3bReactor <vaironfop@Mail.ru>
2022-12-17 22:08:35 +0300

    fix

--- src/components/MainPageComponents/About/index.jsx
+++ src/components/MainPageComponents/About/index.jsx
@@ -15,59 +15,48 @@ export const About = () => {
 			id="about"
 			data-aos="fade-up"
 			data-aos-delay="100"
+			data-aos-offset="400"
 		>
-			<div
-				className="py-24 flex gap-30 justify-between container"
-				id="about"
-				data-aos="fade-up"
-				data-aos-delay="100"
-				data-aos-offset="400"
-			>
-				<div
-					className="max-w-[650px]"
-					data-aos="fade-right"
-					data-aos-delay="100"
-				>
-					<Heading2 className="mb-4 leading-10 dark:text-light duration-500">
-						{about.title}
-					</Heading2>
-					<Body className="mb-3.5 dark:text-light duration-500">
-						{about.descriptions[0].text}
-					</Body>
+			<div className="max-w-[650px]" data-aos="fade-right" data-aos-delay="100">
+				<Heading2 className="mb-4 leading-10 dark:text-light duration-500">
+					{about.title}
+				</Heading2>
+				<Body className="mb-3.5 dark:text-light duration-500">
+					{about.descriptions[0].text}
+				</Body>
 
-					<ul>
-						<li className="flex items-center mb-1.5">
-							<SvgIcon name={'listCheck'} size={25} />
-							<Body className="ml-2.5 dark:text-light duration-500">
-								{about.benefits[0].text}
-							</Body>
-						</li>
-						<li className="flex items-center mb-1.5">
-							<SvgIcon name={'listCheck'} size={25} />
-							<Body className="ml-2.5 dark:text-light duration-500">
-								{about.benefits[1].text}
-							</Body>
-						</li>
-						<li className="flex items-center mb-1.5">
-							<SvgIcon name={'listCheck'} size={25} />
-							<Body className="ml-2.5 dark:text-light duration-500">
-								{about.benefits[2].text}
-							</Body>
-						</li>
-					</ul>
+				<ul>
+					<li className="flex items-center mb-1.5">
+						<SvgIcon name={'listCheck'} size={25} />
+						<Body className="ml-2.5 dark:text-light duration-500">
+							{about.benefits[0].text}
+						</Body>
+					</li>
+					<li className="flex items-center mb-1.5">
+						<SvgIcon name={'listCheck'} size={25} />
+						<Body className="ml-2.5 dark:text-light duration-500">
+							{about.benefits[1].text}
+						</Body>
+					</li>
+					<li className="flex items-center mb-1.5">
+						<SvgIcon name={'listCheck'} size={25} />
+						<Body className="ml-2.5 dark:text-light duration-500">
+							{about.benefits[2].text}
+						</Body>
+					</li>
+				</ul>
 
-					<Body className="dark:text-light duration-500">
-						{about.descriptions[0].text}
-					</Body>
-				</div>
-
-				<img
-					src={aboutMainPage}
-					alt="Команда"
-					data-aos="fade-left"
-					data-aos-delay="100"
-				></img>
+				<Body className="dark:text-light duration-500">
+					{about.descriptions[0].text}
+				</Body>
 			</div>
+
+			<img
+				src={aboutMainPage}
+				alt="Команда"
+				data-aos="fade-left"
+				data-aos-delay="100"
+			></img>
 		</div>
 	);
 };
