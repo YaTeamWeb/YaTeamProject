3d79d39ea2e3ea27cc9a51dcd4a7ea3b1d4c1dd0
Renat <testinggame778@gmail.com>
2022-12-23 19:02:49 +0300

    fd

--- server/main.py
+++ server/main.py
@@ -8,7 +8,9 @@ bot = Bot()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://wondrous-gumdrop-d258e6.netlify.app/"],
+    allow_origins=["https://develop--wondrous-gumdrop-d258e6.netlify.app"
+
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -38,4 +40,4 @@ async def feedback(name: str = Form(),
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5500, reload=True)
+    uvicorn.run("main:app", host="https://develop--wondrous-gumdrop-d258e6.netlify.app", port=5500, reload=True)
--- src/components/MainPageComponents/Contact.jsx
+++ src/components/MainPageComponents/Contact.jsx
@@ -1,169 +1,169 @@
-import React, {useState} from 'react';
-import {SvgIcon} from '../UI/SvgIcon.jsx';
-import {Heading2} from '../UI/Heading2.jsx';
-import {SupTitle} from '../UI/SupTitle.jsx';
-import {useSelector} from 'react-redux';
-import {selectLangItems} from '../../store/slices/lang/selectors.js';
-import {SuccessModal} from '../UI/SuccessModal';
+import React, { useState } from 'react';
+import { SvgIcon } from '../UI/SvgIcon.jsx';
+import { Heading2 } from '../UI/Heading2.jsx';
+import { SupTitle } from '../UI/SupTitle.jsx';
+import { useSelector } from 'react-redux';
+import { selectLangItems } from '../../store/slices/lang/selectors.js';
+import { SuccessModal } from '../UI/SuccessModal';
 
 export const Contact = () => {
-    const {contacts} = useSelector(selectLangItems);
+	const { contacts } = useSelector(selectLangItems);
 
-    const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-    const ControlForm = (event) => {
-        event.preventDefault();
-        const xmlHttp = new XMLHttpRequest();
-        xmlHttp.open('POST', 'feedback', true);
-        xmlHttp.send(new FormData(event.target));
-        xmlHttp.onreadystatechange = function () {
-            if (xmlHttp.readyState === 4) {
-                if (xmlHttp.status === 200) {
-                    setShow(true);
-                    console.log('Success');
-                    console.log(xmlHttp.status);
-                    setTimeout(() => setShow(false), 2000);
-                    event.target.reset();
-                } else {
-                    console.log(xmlHttp.status);
-                    alert('Error');
-                }
-            }
-        };
-    };
+	const ControlForm = (event) => {
+		event.preventDefault();
+		const xmlHttp = new XMLHttpRequest();
+		xmlHttp.open('POST', 'https://develop--wondrous-gumdrop-d258e6.netlify.app:5500/feedback', true);
+		xmlHttp.send(new FormData(event.target));
+		xmlHttp.onreadystatechange = function () {
+			if (xmlHttp.readyState === 4) {
+				if (xmlHttp.status === 200) {
+					setShow(true);
+					console.log('Success');
+					console.log(xmlHttp.status);
+					setTimeout(() => setShow(false), 2000);
+					event.target.reset();
+				} else {
+					console.log(xmlHttp.status);
+					alert('Error');
+				}
+			}
+		};
+	};
 
-    return (
-        <section
-            className="container py-16 px-[30px]"
-            data-aos="fade-up"
-            data-aos-delay="0"
-            id="contacts"
-        >
-            <SuccessModal show={show} text={'Сообщение отправлено'}/>
-            <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
-                <SupTitle>{contacts.suptitle}</SupTitle>
+	return (
+		<section
+			className="container py-16 px-[30px]"
+			data-aos="fade-up"
+			data-aos-delay="0"
+			id="contacts"
+		>
+			<SuccessModal show={show} text={'Сообщение отправлено'} />
+			<div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+				<SupTitle>{contacts.suptitle}</SupTitle>
 
-                <Heading2 className="mb-10 dark:text-light duration-500">
-                    {contacts.title}
-                </Heading2>
-            </div>
+				<Heading2 className="mb-10 dark:text-light duration-500">
+					{contacts.title}
+				</Heading2>
+			</div>
 
-            <div data-aos="zoom-in" data-aos-delay="0" data-aos-offset="300">
-                <iframe
-                    src="https://yandex.ru/map-widget/v1/?um=constructor%3A4b8ce5caeefc0863f905a7c7db4c4cea3a8d9e37252825f96627dd39152296e5&amp;source=constructor"
-                    width="100%"
-                    height="400"
-                    frameBorder="0"
-                ></iframe>
-            </div>
+			<div data-aos="zoom-in" data-aos-delay="0" data-aos-offset="300">
+				<iframe
+					src="https://yandex.ru/map-widget/v1/?um=constructor%3A4b8ce5caeefc0863f905a7c7db4c4cea3a8d9e37252825f96627dd39152296e5&amp;source=constructor"
+					width="100%"
+					height="400"
+					frameBorder="0"
+				></iframe>
+			</div>
 
-            <div className="flex sm:flex-row flex-col sm:pl-[10px] p-0  sm:pt-5 sm:pr-5 sm:pb-5 gap-10">
-                <ul
-                    className="flex flex-col items-start gap-[30px] py-[10px]"
-                    data-aos="fade-right"
-                    data-aos-delay="0"
-                    data-aos-offset="300"
-                >
-                    <li className="flex flex-row gap-5 justify-center items-center">
-                        <div>
-                            <SvgIcon name={'location'} size={50}/>
-                        </div>
-                        <div>
-                            <p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
-                                {contacts.items[0].title}
-                            </p>
-                            <p className="font-normal sm:text-base text-2xl  text-gray-300 font-OpenSans">
-                                {contacts.items[0].description}
-                            </p>
-                        </div>
-                    </li>
-                    <li className="flex flex-row gap-5 justify-start items-center">
-                        <div>
-                            <SvgIcon name={'email'} size={50}/>
-                        </div>
-                        <div>
-                            <p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
-                                {contacts.items[1].title}
-                            </p>
-                            <a
-                                href={'mailto:info@yateam.site'}
-                                className="font-normal sm:text-base text-2xl text-gray-300 hover:text-primary duration-300 font-OpenSans"
-                            >
-                                {contacts.items[1].description}
-                            </a>
-                        </div>
-                    </li>
-                    <li className="flex flex-row gap-5 justify-start items-center">
-                        <div>
-                            <SvgIcon name={'phone'} size={50}/>
-                        </div>
-                        <div>
-                            <p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
-                                {contacts.items[2].title}
-                            </p>
-                            <a
-                                href={'tel:78005553535'}
-                                className="font-normal hover:text-primary duration-300 sm:text-base text-2xl text-gray-300 font-OpenSans font-400"
-                            >
-                                {contacts.items[2].description}
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-                <form
-                    className="p-0 sm:p-[10px] w-full flex flex-col gap-5"
-                    data-aos="fade-left"
-                    onSubmit={ControlForm}
-                    data-aos-delay="0"
-                    data-aos-offset="300"
-                >
-                    <div className="grid grid-cols-2 gap-5">
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder={contacts.form.name}
-                            required
-                            className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300
+			<div className="flex sm:flex-row flex-col sm:pl-[10px] p-0  sm:pt-5 sm:pr-5 sm:pb-5 gap-10">
+				<ul
+					className="flex flex-col items-start gap-[30px] py-[10px]"
+					data-aos="fade-right"
+					data-aos-delay="0"
+					data-aos-offset="300"
+				>
+					<li className="flex flex-row gap-5 justify-center items-center">
+						<div>
+							<SvgIcon name={'location'} size={50} />
+						</div>
+						<div>
+							<p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
+								{contacts.items[0].title}
+							</p>
+							<p className="font-normal sm:text-base text-2xl  text-gray-300 font-OpenSans">
+								{contacts.items[0].description}
+							</p>
+						</div>
+					</li>
+					<li className="flex flex-row gap-5 justify-start items-center">
+						<div>
+							<SvgIcon name={'email'} size={50} />
+						</div>
+						<div>
+							<p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
+								{contacts.items[1].title}
+							</p>
+							<a
+								href={'mailto:info@yateam.site'}
+								className="font-normal sm:text-base text-2xl text-gray-300 hover:text-primary duration-300 font-OpenSans"
+							>
+								{contacts.items[1].description}
+							</a>
+						</div>
+					</li>
+					<li className="flex flex-row gap-5 justify-start items-center">
+						<div>
+							<SvgIcon name={'phone'} size={50} />
+						</div>
+						<div>
+							<p className="font-semibold sm:text-2xl text-4xl dark:text-light mb-[5px] font-Raleway">
+								{contacts.items[2].title}
+							</p>
+							<a
+								href={'tel:78005553535'}
+								className="font-normal hover:text-primary duration-300 sm:text-base text-2xl text-gray-300 font-OpenSans font-400"
+							>
+								{contacts.items[2].description}
+							</a>
+						</div>
+					</li>
+				</ul>
+				<form
+					className="p-0 sm:p-[10px] w-full flex flex-col gap-5"
+					data-aos="fade-left"
+					onSubmit={ControlForm}
+					data-aos-delay="0"
+					data-aos-offset="300"
+				>
+					<div className="grid grid-cols-2 gap-5">
+						<input
+							type="text"
+							name="name"
+							placeholder={contacts.form.name}
+							required
+							className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300
 							border-solid border-2 border-border-input"
-                        ></input>
-                        <input
-                            type="email"
-                            name="email"
-                            required
-                            placeholder={contacts.form.email}
-                            className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300
+						></input>
+						<input
+							type="email"
+							name="email"
+							required
+							placeholder={contacts.form.email}
+							className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300
 							border-solid border-2 border-border-input"
-                        ></input>
-                        <input
-                            type="text"
-                            name="theme"
-                            required
-                            placeholder={contacts.form.theme}
-                            className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans  placeholder:text-gray-300 col-span-2
+						></input>
+						<input
+							type="text"
+							name="theme"
+							required
+							placeholder={contacts.form.theme}
+							className="h-10 p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans  placeholder:text-gray-300 col-span-2
 							border-solid border-2 border-border-input"
-                        ></input>
-                        <textarea
-                            name="message"
-                            placeholder={contacts.form.message}
-                            required
-                            className="h-[137px] resize-none p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300 col-span-2
+						></input>
+						<textarea
+							name="message"
+							placeholder={contacts.form.message}
+							required
+							className="h-[137px] resize-none p-[10px] rounded-[5px] outline-none font-normal text-[12px] xs:text-[14px] font-OpenSans placeholder:text-gray-300 col-span-2
 							border-solid border-2 border-border-input"
-                        ></textarea>
-                    </div>
-                    <div
-                        className="self-center"
-                        data-aos="fade-up"
-                        data-aos-delay="0"
-                        data-aos-offset="100"
-                    >
-                        <input
-                            type="submit"
-                            value={contacts.button}
-                            className="w-[231px] px-[25px] py-[10px] rounded-[4px] bg-primary  text-[14px] xs:text-[16px] hover:bg-hover duration-500 cursor-pointer"
-                        />
-                    </div>
-                </form>
-            </div>
-        </section>
-    );
+						></textarea>
+					</div>
+					<div
+						className="self-center"
+						data-aos="fade-up"
+						data-aos-delay="0"
+						data-aos-offset="100"
+					>
+						<input
+							type="submit"
+							value={contacts.button}
+							className="w-[231px] px-[25px] py-[10px] rounded-[4px] bg-primary  text-[14px] xs:text-[16px] hover:bg-hover duration-500 cursor-pointer"
+						/>
+					</div>
+				</form>
+			</div>
+		</section>
+	);
 };
